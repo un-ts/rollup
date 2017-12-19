9b28f335ec0a772a437a67041e35eb4462d5aeb3
Jiaren Liu <jrliu@alauda.io>
2017-12-20 00:28:45 +0800

    bump dependencies, fix test issue

--- test/transform-expect.js
+++ test/transform-expect.js
@@ -1,7 +1,7 @@
-var template = "<!--$$-->\n<div class=\"test\">\n  <%- msg %>\n</div>\n";
+var template = "<!--$$-->\n<div class=\"test\">\n  <%- msg %>\n</div>\n"
 
 var transform = {
   template
-};
+}
 
 export default transform;
