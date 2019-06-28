49e8209f0bf8413fb8b41ca6b81d5b1d8686653b
Daniel Cohen Gindi <danielgindi@gmail.com>
2019-06-28 12:59:43 +0300

    Play nice with sourcemaps

--- insert.js
+++ insert.js
@@ -1,23 +1,31 @@
 import { createFilter } from 'rollup-pluginutils'
+import MagicString from 'magic-string'
 
 export const transform = (insert, options = {}) => {
   const filter = createFilter(options.include, options.exclude)
+  const sourceMap = options.sourceMap !== false
+
   return {
     name: 'insert',
     transform(code, id) {
       if (!filter(id)) return
+
+      const magicString = new MagicString(code)
+      insert(magicString, id)
+
       return {
-        code: insert(code, id),
+        code: magicString.toString(),
+        map: sourceMap ? magicString.generateMap() : null,
       }
     },
   }
 }
 
 export const append = (append, options) =>
-  transform(code => code + append, options)
+  transform(magicString => magicString.append(append), options)
 
 export const prepend = (prepend, options) =>
-  transform(code => prepend + code, options)
+  transform(magicString => magicString.prepend(prepend), options)
 
 export const wrap = (begin, end, options) =>
-  transform(code => begin + code + end, options)
+  transform(magicString => magicString.prepend(begin).append(end), options)
